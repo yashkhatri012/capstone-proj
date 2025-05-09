d7480dd8bb1ea2cc0d4c28dc65759931003ed1e1
yashkhatri012 <yashkhatri88540@gmail.com>
2025-05-09 16:26:37 +0530

    Update data_ingestion.py

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -75,9 +75,8 @@ def main():
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
         
-        df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
-        # df = s3.fetch_file_from_s3("data.csv")
+        df = load_data(data_url='https://raw.githubusercontent.com/yashkhatri012/capstone-proj/blob/main/notebooks/data.csv')
+       
 
 
 
